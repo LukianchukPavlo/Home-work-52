8f472cfde0e1ab9ca06a9b0e531cb4e3864acbce
Павло Лук'янчук <trane1996162@gmail.com>
2026-04-23 11:16:13 +0300

    add tasks

--- src/app/store/api/tasks.ts
+++ src/app/store/api/tasks.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { type IDeleteTaskResponce, type ITask, type UpdateTaskPayload, type WorkflowCode } from "../../../interfaces";
 
-const baseUrl = "http://localhost:3001";
+const baseUrl = "http://localhost:3000/api/v1/tasks";
 
 export const tasksApi = createApi({
   reducerPath: "tasksApi",
